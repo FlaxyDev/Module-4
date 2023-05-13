ae27efef1cd3449e5e72984791c7257e85edf6c4
Diak Maksym <55993687+FlaxyDev@users.noreply.github.com>
2023-05-13 12:58:59 +0300

    Add files via upload

---
+++ Lesson.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    [Serializable]
+    public class Lesson : IComparable<Lesson>
+    {
+        public string Subject { get; set; }
+
+        public string Teacher { get; set; }
+
+        public string Group { get; set; }
+
+        public Lesson(string subject, string teacher, string group)
+        {
+            Subject = subject;
+            Teacher = teacher;
+            Group = group;
+        }
+
+        public int CompareTo(Lesson other)
+        {
+            if (Subject != other.Subject)
+            {
+                return Subject.CompareTo(other.Subject);
+            }
+            else if (Teacher != other.Teacher)
+            {
+                return Teacher.CompareTo(other.Teacher);
+            }
+            else
+            {
+                return Group.CompareTo(other.Group);
+            }
+        }
+
+        public static string Serialize(List<Lesson> lessons)
+        {
+            MemoryStream stream = new MemoryStream();
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, lessons);
+            return Convert.ToBase64String(stream.ToArray());
+        }
+
+        public static List<Lesson> Deserialize(string data)
+        {
+            MemoryStream stream = new MemoryStream(Convert.FromBase64String(data));
+            IFormatter formatter = new BinaryFormatter();
+            return (List<Lesson>)formatter.Deserialize(stream);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створюємо список пар
+            List<Lesson> lessons = new List<Lesson>();
+            lessons.Add(new Lesson("Math", "Ivanov", "Am-1"));
+            lessons.Add(new Lesson("Physics", "Petro", "KNS-22"));
+            lessons.Add(new Lesson("OOP", "Volynets", "KNS"));
+
+            // Сортуємо список
+            lessons.Sort();
+
+            // Виводимо список
+            Console.WriteLine("List:");
+            foreach (Lesson lesson in lessons)
+            {
+                Console.WriteLine("{0}, {1}, {2}", lesson.Subject, lesson.Teacher, lesson.Group);
+            }
+            // Серіалізуємо список
+            string serialized = Lesson.Serialize(lessons);
+
+            Console.WriteLine("\nSerealized:\n" + serialized);
+
+            // Десеріалізуємо список
+            List<Lesson> deserialized = Lesson.Deserialize(serialized);
+
+            Console.WriteLine("\nDeserialized List:");
+            foreach (Lesson lesson in deserialized)
+            {
+                Console.WriteLine("{0}, {1}, {2}", lesson.Subject, lesson.Teacher, lesson.Group);
+            }
+        }
+    }
+}
